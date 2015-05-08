6ef00f7cce7f503bad6beda74bda90908d792f14
drabelpdx <drabelpdx@gmail.com>
2015-05-08 08:25:56 -0700

    Stepping through CRUD for Client class. Adds initialize and save methods to Client class.

--- lib/client.rb
+++ lib/client.rb
@@ -1,2 +1,14 @@
 class Client
+
+  attr_reader(:name, :id)
+
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  define_method(:save) do
+     result = DB.exec("INSERT INTO clients (name) VALUES ('#{@name}') RETURNING id;")
+     @id = result.first().fetch("id").to_i()
+   end
 end
---
+++ spec/client_spec.rb
@@ -0,0 +1,19 @@
+require("spec_helper")
+
+describe(Client) do
+
+  describe("#name") do
+    it("returns the clients's name") do
+      client = Client.new({:name => "Bob", :id => nil})
+      expect(client.name()).to(eq("Bob"))
+    end
+  end
+
+  describe('#id') do
+    it('returns the id')do
+    client = Client.new ({:name => "Bob", :id => 1})
+    expect(client.id()).to(eq(1))
+    end
+  end
+
+end
